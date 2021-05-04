b83c1296a281049d4134fbdb5b764669c2cebb5b
Bruno Gaefke <brunogaefke@gmail.com>
2021-05-03 21:45:08 -0300

    implementing calculator

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string FirstNumber, string SecondNumber)
+        {
+            if (IsNumeric(FirstNumber) && (IsNumeric(SecondNumber)))
+            {
+                var sum = ConvertToDecimal(FirstNumber) + ConvertToDecimal(SecondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool IsNumber = double.TryParse(strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+            return IsNumber;
+        }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal DecimalValue;
+            if(decimal.TryParse(strNumber, out DecimalValue))
+            {
+                return DecimalValue;
+            }
+            return 0;
+        }
+
+    }
+}
