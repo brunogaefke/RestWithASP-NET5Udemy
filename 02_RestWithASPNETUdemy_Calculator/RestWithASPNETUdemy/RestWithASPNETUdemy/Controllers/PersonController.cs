ef13c72fb1f49f0b0070f3080d11dccb6bb79218
Bruno Gaefke <brunogaefke@gmail.com>
2021-05-04 14:45:15 -0300

    working with four verbs

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string FirstNumber, string SecondNumber)
+        {
+             
+            return BadRequest("Invalid Input");
+        }
+
+    }
+}
---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set;}
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String Gender { get; set; }
+
+    }
+}
